8235e0a61f0c906bb0d4bac0fbb3479dd980a3de
Meriam Lachkar <meriam.lachkar@gmail.com>
2014-12-02 21:06:33 +0000

    ./tester name nombre de fois

--- stresstester/tester.c
+++ stresstester/tester.c
@@ -4,13 +4,15 @@
 #include "../liblog/liblog.h"
 
 int main(int argc, char* argv[]) {
-
-    if (argc != 2) {
-        printf("Usage: %s name\n", argv[0]);
+	int i;
+    if (argc != 3) {
+        printf("Usage: %s name & integer \n", argv[0]);
         exit(1);
     }
-
-    mylog(argv[1], "A different kind of Hello world ... ");
-
+    int imax = atoi(argv[2]);
+	for (i = 0 ; i < imax ; i++)
+	{
+		mylog(argv[1], "A different kind of Hello world ... ");
+	}
     return 0;
 }
